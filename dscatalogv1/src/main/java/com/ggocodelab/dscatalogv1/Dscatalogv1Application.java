beb383edc2051df3a30001306ac4637bc0fd9ed1
Gillian Oliveira <brgillian@gmail.com>
2026-04-20 09:54:33 -0300

    chore: initial spring boot project setup

---
+++ dscatalogv1/src/main/java/com/ggocodelab/dscatalogv1/Dscatalogv1Application.java
@@ -0,0 +1,13 @@
+package com.ggocodelab.dscatalogv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatalogv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatalogv1Application.class, args);
+	}
+
+}
