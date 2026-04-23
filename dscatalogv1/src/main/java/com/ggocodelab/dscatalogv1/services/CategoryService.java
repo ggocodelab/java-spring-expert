17ead60bb47a4a5c7f3cbe36ce89eb4abdf15c01
Gillian Oliveira <brgillian@gmail.com>
2026-04-23 16:52:17 -0300

    feat(category): add pagination to findAll endpoint

--- dscatalogv1/src/main/java/com/ggocodelab/dscatalogv1/services/CategoryService.java
+++ dscatalogv1/src/main/java/com/ggocodelab/dscatalogv1/services/CategoryService.java
@@ -1,10 +1,11 @@
 package com.ggocodelab.dscatalogv1.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +24,9 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> result = repository.findAll();
-		return result.stream().map(x -> new CategoryDTO(x)).toList();
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> result = repository.findAll(pageRequest);
+		return result.map(x -> new CategoryDTO(x));
 	}
 		
 	@Transactional(readOnly = true)
@@ -62,5 +63,7 @@ public class CategoryService {
 	    	catch (DataIntegrityViolationException e) {
 	    		throw new DatabaseException("Integrity violation.");
 	   	}
-	}	
+	}
+
+		
 }
