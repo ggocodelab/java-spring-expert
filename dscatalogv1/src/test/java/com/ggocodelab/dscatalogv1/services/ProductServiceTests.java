cf33ec600b1563fb0ecaf2de92106ae5d04f0500
Gillian Oliveira <brgillian@gmail.com>
2026-04-25 15:31:41 -0300

    test(product): add delete scenarios for existing and non-existing ids

---
+++ dscatalogv1/src/test/java/com/ggocodelab/dscatalogv1/services/ProductServiceTests.java
@@ -0,0 +1,60 @@
+package com.ggocodelab.dscatalogv1.services;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.ggocodelab.dscatalogv1.exceptions.ResourceNotFoundException;
+import com.ggocodelab.dscatalogv1.reporitories.ProductRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTests {
+	
+	@InjectMocks
+	private ProductService service;
+	
+	@Mock
+	private ProductRepository repository;
+	
+	private long existingId;
+	private long nonExistingId;
+	
+	@BeforeEach
+	void setUp() {
+		existingId = 1L;
+		nonExistingId = 1000L;
+	}
+	
+	@Test
+	public void deleteShouldDoNothingWhenIdExists() {
+		
+		when(repository.existsById(existingId)).thenReturn(true);
+		
+		Assertions.assertDoesNotThrow(() -> {
+	        service.delete(existingId);
+	    });
+		 Mockito.verify(repository).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteShouldThrowExceptionWhenIdDoesNotExist() {
+
+	    when(repository.existsById(nonExistingId)).thenReturn(false);
+
+	    Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+	        service.delete(nonExistingId);
+	    });
+
+	    verify(repository, never()).deleteById(any());
+	}
+}
